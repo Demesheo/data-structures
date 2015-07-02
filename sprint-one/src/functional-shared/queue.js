adc68f572c64f54590e6b7cb53ed705e98319bcc
demesheo <d.wilsonii91@gmail.com>
2015-07-02 11:42:38 -0700

    completed functional shared stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,32 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {
+  	size: 0
+  };
+
+  extend(queue, queueMethods);
+
+  return queue;
 };
 
-var queueMethods = {};
+var extend = function(to, from) {
+	for (var key in from) {
+		to[key] = from[key];
+	}
+};
+
+var queueMethods = {
+
+	size: function(){
+		return this.size;
+	};
+
+
+
+
+
+};
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,39 +1,56 @@
-  var extend = function(copyTo, copyFrom) {
-  	for (var property in copyFrom) {
-  		copyTo[property] = copyFrom[property];
-  	}
-  };
+var Stack = function() {
+// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+// but try not not reference your old code in writing the new style.`
 
+var stackMaker = {
+	capacity : 0
+};
 
-  var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  var stack = {};
-  stack.capacity = 0;
+extend(stackMaker, stackMethods);
 
-  extend(stack, stackMethods);
-  return stack; 
+return stackMaker; 
+
+};
+
+// var extend = function(copyTo, copyFrom) {
+// 	for (var property in copyFrom) {
+// 		copyTo[property] = copyFrom[property];
+// 	}
+// };
+
+var extend = function(to, from) {
+	for (var key in from) {
+		to[key] = from[key];
+	}
 };
 
 var stackMethods = {
-	
+
+// stackMethods.size = function() {
+// 	return this.capacity;
+// }
+
+// stackMethods.push = function (value) {
+// 	this.capacity++;
+// 	this[this.capacity] = value;
+
 	size: function(){
 		return this.capacity;
 	},
 
 	pop: function() {
-		var popped = this[capacity];
-		delete this[capacity];
-		this.capacity--;
+		var popped = this[this.capacity];
+		delete this[this.capacity];
+		if (this.capacity > 0) {
+			this.capacity--;
+		};
 		return popped;
 	},
 
 
 	push: function (value) {
 		this.capacity++;
-		this[capacity] = value;
-
-
+		this[this.capacity] = value;
 	}
 
 };
