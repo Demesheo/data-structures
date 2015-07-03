c60c6416fbc5f1d3df37028bc2e95630828ade49
demesheo <d.wilsonii91@gmail.com>
2015-07-03 12:33:24 -0700

    almost finished with linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var temped = Node(value);
+    // list.tail  = temped;
+    // list[value] = Node(value);
+    
+    if (list.head === null) {
+      list.head = temped;
+    } ;
+    if (list.tail){
+      list.tail.next = temped;
+    };
+    list.tail = temped;
+
   };
 
+
+// we want to add a node to our linkedlist
+// if there is no head node (head === null), this value will also be the head node
+// we do not want to create two nodes, if there is no head, we only want to create a reference
+// on our first instance, we want to set our head node's next value to be our tail node
+// we want to create a reference to this node so that the next node we add will be the former's next value
+
   list.removeHead = function(){
+    console.log("head to be removed: ", list.head);
+    var oldHead = list.head.value;
+    list.head = list.head.next;
+    return oldHead;
+
   };
 
   list.contains = function(target){
+    if(list[target]){
+      return true;
+    } else {return false}
+
+
   };
 
   return list;
