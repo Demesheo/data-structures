312bbd2cf9f0cbbf508d5e0c037823e923a85415
demesheo <d.wilsonii91@gmail.com>
2015-07-04 11:04:30 -0700

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = Object.create(null);
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this._storage[item] = item;
+
 };
 
 setPrototype.contains = function(item){
+	return _.contains(this._storage, item)
+
 };
 
 setPrototype.remove = function(item){
+	var removed = this._storage[item];
+	delete this._storage[item];
+	return removed;
+
 };
 
 /*
