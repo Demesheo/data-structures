993f6197f17857bd8eab479c27b09ae31e1d279e
demesheo <d.wilsonii91@gmail.com>
2015-07-04 16:25:37 -0700

    hash table complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,18 +1,40 @@
 var HashTable = function(){
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+	this._limit = 8;
+	this._storage = LimitedArray(this._limit);
 };
 
+
+
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+	var i = getIndexBelowMaxForKey(k, this._limit);
+  // set the key and value pair as an object to be passed in with our set function
+  // put that object in our storage array by passing our object as a value in set	
+  // create var dataSet = {k : v}
+  // this._storage.set(i, dataSet)
+  var storedAt = this._storage.get(i);
+  if(storedAt){
+  	storedAt[k] = v;
+  } else {
+  	var dataSet = {};
+  	dataSet[k] = v  
+  	this._storage.set(i, dataSet);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+	var i = getIndexBelowMaxForKey(k, this._limit); 
+	var storedAt = this._storage.get(i);
+	console.log(storedAt)
+	return storedAt[k];
+
 
 };
 
 HashTable.prototype.remove = function(k){
+	var i = getIndexBelowMaxForKey(k, this._limit);
+	var storedAt = this._storage.get(i);
+	storedAt[k] = null;
+	
 
 };
 
